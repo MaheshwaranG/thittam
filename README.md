7354c7e59a3583799a2203a7d41f060e85b759ce
Maheshwaran <gmaheshwaranit@gmail.com>
2022-12-07 23:09:15 +0530

    initialize

---
+++ README.md
@@ -0,0 +1,1 @@
+# THITTAM Project
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>This is simple app</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Lets develop new app.
+      </p>
+    </div>
+  )
+}
+
+export default App
