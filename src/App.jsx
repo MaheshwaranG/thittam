d130809cfb46dca97355a1e49e62cb4940b3f584
Maheshwaran <gmaheshwaranit@gmail.com>
2023-07-30 14:24:17 +0530

    added workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>This is simple app</h1>
+      <h1>This is simple app using React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -16,11 +16,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Lets develop new app.
-      </p>
+      <p className="read-the-docs">Lets develop new app.</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
