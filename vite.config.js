edd66da5a607d093d1d0fdb2d4435de1e6f96ac4
Maheshwaran <gmaheshwaranit@gmail.com>
2023-08-04 08:49:17 +0530

    changes for self host runner

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+// const ASSET_URI = import.meta.env.VITE_ASSET_URI || "/dist";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+// export default ({ mode }) => {
+//   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+//   const ASSET_URI = process.env.VITE_ASSET_URI || "./dist/";
+//   console.log(ASSET_URI);
+//   return defineConfig({
+//     plugins: [react()],
+//     base: ASSET_URI,
+//     build: {
+//       outDir: ASSET_URI,
+//     },
+//   });
+// };
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const ASSET_URI = env.VITE_ASSET_URI || "./dist/";
+  console.log(ASSET_URI);
+  return {
+    plugins: [react()],
+    base: [...ASSET_URI].slice(1).join(""),
+    build: {
+      outDir: ASSET_URI,
+    },
+  };
+});
