a83ad3ebd98bc6289f3b668d69743466af43c79c
Maheshwaran <gmaheshwaranit@gmail.com>
2023-08-05 17:46:03 +0530

    GA workflow working

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hey we almost done</h1>
+      <h1>Hey we almost done, its seems working</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
