678138241509f9e44a62cd36eec318a97fc4e24a
Maheshwaran <gmaheshwaranit@gmail.com>
2023-08-05 17:12:14 +0530

    check server response after build

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>This is simple app using React Test pull Req 5-08-2023 </h1>
+      <h1>Hey Learn github actions </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
